997344dcae8c513170578a60d7aa9f374cf900f2
Neil Sebbey <neil.p.sebbey@students.ittralee.ie>
2022-05-11 14:25:23 +0100

    New commit, 14:25, May 11, 2022. Comments and view features are functional at the very least.

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -46,14 +46,16 @@ def create():
     return render_template('blog/create.html')
 
 
-@bp.route('/createComment', methods=('GET', 'POST'))
+@bp.route('/<int:id>/createComment', methods=('GET', 'POST'))
 @login_required
-def createComment():
+def createComment(id):
+    post = get_post(id)
+
     if request.method == 'POST':
         comment = request.form['comment']
         error = None
 
-        if not body:
+        if not comment:
             error = 'Comment is required.'
 
         if error is not None:
@@ -63,15 +65,15 @@ def createComment():
             db.execute(
                 'INSERT INTO comment (comment, post_id, author_id)'
                 ' VALUES (?, ?, ?)',
-                (comment, g.post['id'], g.user['id'])
+                (comment, post['id'], g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
 
-    return render_template('blog/createComment.html')
+    return render_template('blog/createComment.html', post=post)
 
 
-def get_post(id, check_author=True):
+def get_post(id):   #(..., check_author=True)
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
@@ -86,6 +88,32 @@ def get_post(id, check_author=True):
     #    abort(403)
 
     return post
+
+def get_comment(id):
+    comment = get_db().execute(
+        'SELECT c.id, comment, post_id, created, author_id, username'
+        ' FROM comment c JOIN user u ON c.author_id = u.id'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE c.id = ?',
+        (id,)
+    ).fetchone()
+
+    if comment is None:
+        abort(404, f"Comment id {id} doesn't exist.")
+
+    return comment
+
+def get_comments(id):
+    db = get_db()
+    comments = db.execute(
+        'SELECT c.id, comment, c.post_id, c.created, c.author_id, username'
+        ' FROM comment c JOIN user u on c.author_id = u.id'
+        ' WHERE c.post_id = ?'
+        ' ORDER BY c.created DESC',
+        (id,)
+    ).fetchall()
+
+    return comments
 
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
@@ -119,7 +147,8 @@ def update(id):
 @bp.route('/<int:id>/viewPost')
 def viewPost(id):
     post = get_post(id)
-    return render_template('blog/viewPost.html', post=post)
+    comments = get_comments(id)
+    return render_template('blog/viewPost.html', post=post, comments=comments)
 
 
 
